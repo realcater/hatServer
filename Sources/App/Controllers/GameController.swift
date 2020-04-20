2351925b2ce2d48b821b5f84a7f1faa582267045
realcater <dmitryd@bk.ru>
2020-04-20 22:02:25 +0300

    Add ACCEPT_GAME and check if a user is in this game

--- Sources/App/Controllers/GameController.swift
+++ Sources/App/Controllers/GameController.swift
@@ -13,9 +13,9 @@ struct GameController: RouteCollection {
         
         tokenAuthRoutes.post(use: create)
         tokenAuthRoutes.post(":gameID","accept", use: acceptGame)
-        tokenAuthRoutes.get(":gameID","update", use: getUpdate)
-        tokenAuthRoutes.post(":gameID","update", use: setUpdate)
-        tokenAuthRoutes.get("mine", use: getMyGames)
+        tokenAuthRoutes.get(":gameID", use: getGame)
+        tokenAuthRoutes.post(":gameID","update", use: updateGame)
+        tokenAuthRoutes.get("mine", use: getMyGamesList)
     }
     
     func getAll(_ req: Request) throws -> EventLoopFuture<[Game]> {
@@ -44,30 +44,38 @@ struct GameController: RouteCollection {
             .transform(to: .ok)
     }
     
-    func acceptGame(_ req: Request) throws -> EventLoopFuture<GameData> {
-        //let user = try req.auth.require(User.self)
+    func acceptGame(_ req: Request) throws -> EventLoopFuture<HTTPStatus>{
+        let user = try req.auth.require(User.self)
         return Game.find(req.parameters.get("gameID"), on: req.db)
-            .unwrap(or: Abort(.notFound)).map { game in
+            .unwrap(or: Abort(.notFound))
+            .flatMap { game in
                 let gameData = try! JSONDecoder().decode(GameData.self, from: game.data)
-                return gameData }
+                guard gameData.players.map({ $0.id }).contains(user.id) else {
+                    return req.eventLoop.makeFailedFuture(Abort(.forbidden))
+                }
+                return UserGame(userID: user.id!, gameID: game.id!, accepted: true).save(on: req.db).transform(to: .ok)
+        }
     }
     
-    func getUpdate(_ req: Request) throws -> EventLoopFuture<GameData> {
+    func getGame(_ req: Request) throws -> EventLoopFuture<GameData> {
         return Game.find(req.parameters.get("gameID"), on: req.db)
             .unwrap(or: Abort(.notFound)).map { game in
                 let gameData = try! JSONDecoder().decode(GameData.self, from: game.data)
                 return gameData }
     }
-    func setUpdate(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        //let user = try req.auth.require(User.self)
+    func updateGame(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let user = try req.auth.require(User.self)
         let gameData = try req.content.decode(GameData.self)
         return Game.find(req.parameters.get("gameID"), on: req.db)
             .unwrap(or: Abort(.notFound)).flatMap { game in
+                guard gameData.players.map({ $0.id }).contains(user.id) else {
+                    return req.eventLoop.makeFailedFuture(Abort(.forbidden))
+                }
                 game.data = try! JSONEncoder().encode(gameData)
                 return game.save(on: req.db).transform(to: HTTPStatus.ok)
         }
     }
-    func getMyGames(_ req: Request) throws -> EventLoopFuture<[Game]> {
+    func getMyGamesList(_ req: Request) throws -> EventLoopFuture<[Game]> {
         let userID = try req.auth.require(User.self).id
         //return UserGame.query(on: req.db).filter(\.$user.$id == userID!).all()
         return User
