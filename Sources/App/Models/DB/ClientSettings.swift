8afcf89059dda2e1e38e89567b2de01473b5596e
realcater <dmitryd@bk.ru>
2020-05-28 15:09:57 +0300

    Add minimumAppVersion field to ClientSettings. ServerSettings Schema renamed to ClientSettings.

--- Sources/App/Models/DB/ClientSettings.swift
+++ Sources/App/Models/DB/ClientSettings.swift
@@ -2,7 +2,7 @@ import Fluent
 import Vapor
 
 final class ClientSettings: Model, Content {
-    static let schema = "serverSettings"
+    static let schema = "clientSettings"
     
     @ID(key: .id) var id: UUID?
     @Field(key: "updatePlayersStatus") var updatePlayersStatus: Double
@@ -10,15 +10,17 @@ final class ClientSettings: Model, Content {
     @Field(key: "checkOffline") var checkOffline: Double
     @Field(key: "updateFrequent") var updateFrequent: Double
     @Field(key: "updateFullTillNextTry") var updateFullTillNextTry: Double
+    @Field(key: "minimumAppVersion") var minimumAppVersion: Int
     
     init() { }
     
-    init(id: UUID? = nil, updatePlayersStatus: Double, updateGameList: Double, checkOffline: Double, updateFrequent: Double, updateFullTillNextTry: Double) {
+    init(id: UUID? = nil, updatePlayersStatus: Double, updateGameList: Double, checkOffline: Double, updateFrequent: Double, updateFullTillNextTry: Double, minimumAppVersion: Int) {
         self.updatePlayersStatus = updatePlayersStatus
         self.updateGameList = updateGameList
         self.checkOffline = checkOffline
         self.updateFrequent = updateFrequent
         self.updateFullTillNextTry = updateFullTillNextTry
+        self.minimumAppVersion = minimumAppVersion
     }
     
     
@@ -26,18 +28,19 @@ final class ClientSettings: Model, Content {
 extension ClientSettings {
     struct ClientSettingsMigration: Migration {
         func prepare(on database: Database) -> EventLoopFuture<Void> {
-            return database.schema("serverSettings")
+            return database.schema("clientSettings")
                 .id()
                 .field("updatePlayersStatus", .double, .required)
                 .field("updateGameList", .double, .required)
                 .field("checkOffline", .double, .required)
                 .field("updateFrequent", .double, .required)
                 .field("updateFullTillNextTry", .double, .required)
+                .field("minimumAppVersion", .int, .required)
                 .create()
         }
 
         func revert(on database: Database) -> EventLoopFuture<Void> {
-            return database.schema("serverSettings").delete()
+            return database.schema("clientSettings").delete()
         }
     }
 }
